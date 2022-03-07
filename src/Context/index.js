6e533eddbf9b5da3b33a89c7e42f2db3924bcf22
mike.mo <mikemo@mikemos-MacBook-Pro.local>
2022-03-07 14:42:42 +1100

    add context classes

---
+++ src/Context/index.js
@@ -0,0 +1,4 @@
+import {loginUser, logout} from './actions'
+import { AuthProvder, useAuthDispatch, useAuthState } from './context'
+
+export {AuthProvder, useAuthState, useAuthDispatch, loginUser, logout}
